994f36824a11070e54e2d770c6bfbd6b7439bce8
Tobias Schramm <tobias@t-sys.eu>
2021-04-24 20:21:46 +0200

    Fix low power mode configuration
    MCU now actulally enters shutdown mode, instead of just executing wfi
    
    Power consumption in shutdown without patch: ~350µA
    Power consumption in shutdown with patch: ~6µA

--- stm32-smolmatrix/sleep.c
+++ stm32-smolmatrix/sleep.c
@@ -1,5 +1,6 @@
 #include <libopencm3/cm3/scb.h>
 #include <libopencm3/stm32/pwr.h>
+#include <libopencm3/stm32/rcc.h>
 #include <libopencmsis/core_cm3.h>
 
 #include "sleep.h"
@@ -8,9 +9,14 @@
 #define WFI() __asm__ volatile("wfi")
 
 void sleep_enter_shutdown(void) {
+	rcc_periph_clock_enable(RCC_PWR);
+	rcc_periph_reset_pulse(RST_PWR);
+
 	SCB_SCR |= SCB_SCR_SLEEPDEEP;
+
 	PWR_CR1 &= ~PWR_CR1_LPMS_MASK;
 	PWR_CR1 |= PWR_CR1_LPMS_SHUTDOWN << PWR_CR1_LPMS_SHIFT;
-	PWR_SR1 &= ~(PWR_SR1_WUF1 | PWR_SR1_WUF2 | PWR_SR1_WUF3 | PWR_SR1_WUF4 | PWR_SR1_WUF5);
-	WFI();	
+	PWR_CR3 &= ~PWR_CR3_APC;
+	PWR_SCR = PWR_SCR_CWUF1 | PWR_SCR_CWUF2 | PWR_SCR_CWUF3 | PWR_SCR_CWUF4 | PWR_SCR_CWUF4;
+	while (1) WFI();
 }
